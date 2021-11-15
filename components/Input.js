ce7a59b4fcc1e4c204aed889f5ee26b709f43146
Aadhitya A <aadhitya864@gmail.com>
2021-11-15 11:16:10 +0530

    Refine pages

--- components/Input.js
+++ components/Input.js
@@ -1,20 +1,58 @@
-import  { useState } from 'react';
+import React, { useState } from 'react';
 //import DataTable from 'react-data-table-component';
 import{
   UploadIcon
   } from "@heroicons/react/outline"
 
+import axios from 'axios'
+
 export default function Input() {
-  const [selectedFile, setSelectedFile] = useState();
+  const [setSelectedFile] = useState('')
 
+  let onFileChange = event => {(
+    setSelectedFile = event.target.files[0]
+  )}
 
- const handleFileUpload = e => {
-  setSelectedFile(event.target.files[0]);
-  setIsSelected(true);
-}
+  let onFileUpload = () => {
+    const data = new FormData();
+    data.append("data", setSelectedFile, setSelectedFile.name);
+    console.log(setSelectedFile);
+    axios.post("api/uploadfile", data);
+  };
+
+  let fileData = () => {
+    if(setSelectedFile) {
+      return(
+        <div className="grid place-items-center py-24 bg-[#C7D2FE] h-screen bg-opacity-25">
+      <h4 className="  text-black text-3xl flex flex-grow mx-20  items-start font-semibold text-center justify-center ">A STOCK PREDICTING MODEL BASED ON CNN-LSTM </h4>
+      <h2 className="  text-black text-xl flex flex-grow mx-20  items-start font-semibold text-center justify-center ">
+   
+      Get started by uploading your data file (in CSV/Excel format)!!! </h2>
+    <div className=" flex flex-col group m-auto items-center  justify-center w-auto h-96">
+
+     <div className=" flex flex-row flex-none group  items-center bg-[#95ee7f] bg-opacity-50 justify-center whitespace:nowrap rounded-lg "> 
+     <UploadIcon className=" h-8 group-hover:animate-bounce "/> 
+    <input 
+        type="file"
+        id="file-upload"
+        name=""
+        accept=".csv,.xlsx,.xls"
+        onChange={handleFileUpload}
+        value={onFileChange}
+      /> </div>
+        <div className="">
+           <button 
+           type="submit"
+           className="bg-[#F5D0FE]  rounded-lg py-3 px-6 hover:bg-[#D946EF] focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"
+           onClick={onFileUpload}> Get Prediction</button>
+  </div> 
+  </div> 
+  </div>
+      )
+    }
+  };
 const handleSubmission = e => {
- 
-  
+  console.log("Error!");
 }
 
   return (
@@ -32,7 +70,7 @@ const handleSubmission = e => {
         id="file-upload"
         name=""
         accept=".csv,.xlsx,.xls"
-        onChange={handleFileUpload}
+        onChange={onFileChange}
       /> </div>
         <div className="">
            <button 
--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,39 @@
 import Layout from '../components/Layout'
+import styles from '../styles/Home.module.css'
+import Link from 'next/dist/client/link'
 
 export default function About() {
     return (
-     
       <div >
-         <Layout/>
-      <h1 className= "py-60">About </h1>
+        <div className={styles.container}>
+            <h1 class="text-3xl">About</h1>
+            <p>Stock-X is a capstone project which is mainly focused on Fintech. The main objective of this project is to analyse any kind of stock market data (from any company and market) and then provide prediction and suggestions to stockholders to invest in a specfic company</p>
+            <p>The prediction is made using a CNN-LSTM Neural Network model, designed to obtain features from the data and then obtain the features to train on them.
+              <br/>NOTE: The project is still on progress!
+            </p>
+            <Link href="/">
+                <a className={styles.btn}>Try it</a>
+            </Link>
+            </div>
+            <h1 className={styles.title}>Members</h1>
+            <div class="grid grid-cols-2 gap-4 p-16">
+              <a href="https://github.com/alphaX86" class="block p-6 bg-white hover:bg-blue-400 shadow-md border border-gray-200 rounded-lg max-w-sm">
+                <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2">Aadhitya A</h5>
+                <p class="font-normal text-gray-700">2019506001</p>
+              </a>
+              <a href="https://github.com/RajapriyaRaju" class="block p-6 bg-white hover:bg-blue-400 shadow-md border border-gray-200 rounded-lg max-w-sm">
+                <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2">Rajapriya R</h5>
+                <p class="font-normal text-gray-700">2019506071</p>
+              </a>
+              <a href="https://github.com/vrs16" class="block p-6 bg-white hover:bg-blue-400 shadow-md border border-gray-200 rounded-lg max-w-sm">
+                <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2">Vineetha R S</h5>
+                <p class="font-normal text-gray-700">2019506115</p>
+              </a>
+              <a href="https://github.com/Yellowflash1412" class="block p-6 bg-white hover:bg-blue-400 shadow-md border border-gray-200 rounded-lg max-w-sm">
+                <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2">Anurag Bagde</h5>
+                <p class="font-normal text-gray-700">2019506120</p>
+              </a>
+            </div>  
   </div> 
   
   )
